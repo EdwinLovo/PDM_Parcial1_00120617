876577520e7fca178901e2434dfa93eb056184ad
EdwinLovo <00120617@uca.edu.sv>
2019-06-02 08:50:16 -0600

    Creation of app navigation using Navigation Editor

---
+++ app/src/main/java/com/pdm/parcial1/Fragments/NewGameFragment.kt
@@ -0,0 +1,23 @@
+package com.pdm.parcial1.Fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.pdm.parcial1.R
+
+class NewGameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_new_game, container, false)
+    }
+
+
+}
